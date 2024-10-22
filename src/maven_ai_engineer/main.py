b40bc7f3847e541bd70bb15b7bde61f95a41b572
Andrew Grangaard <granny-github@ofb.net>
2024-10-22 11:25:37 -0700

    Adds first /chat command

--- src/maven_ai_engineer/main.py
+++ src/maven_ai_engineer/main.py
@@ -8,7 +8,7 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 load_dotenv()  # take environment variables from .env.
 
-openai = OpenAI(api_key=os.environ["OPENAI_API_KEY"])  # This is new!
+openai = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
 tg_bot_token = os.getenv("TG_BOT_TOKEN")
 
 
@@ -26,10 +26,25 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
         chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!"
     )
 
+async def chat(update: Update, context: ContextTypes.DEFAULT_TYPE):
+  messages.append({"role": "user", "content": update.message.text})
+  completion = openai.chat.completions.create(model="gpt-4o-mini",
+                                              messages=messages)
+  completion_answer = completion.choices[0].message
+  messages.append(completion_answer)
 
-application = ApplicationBuilder().token(tg_bot_token).build()
+  await context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text=completion_answer.content)
 
-start_handler = CommandHandler("start", start)
-application.add_handler(start_handler)
 
-application.run_polling()
+def main():
+    application = ApplicationBuilder().token(tg_bot_token).build()
+
+    start_handler = CommandHandler("start", start)
+    chat_handler = CommandHandler("chat", chat)
+    application.add_handler(start_handler)
+    application.add_handler(chat_handler)
+
+    application.run_polling()
+
+main()
