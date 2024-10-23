4b1e7107c02cfff720c21c24e8372ce9f20d6b3b
Andrew Grangaard <granny-github@ofb.net>
2024-10-22 21:17:03 -0700

    Break out _chat for testing openai
     (actual problem: verizon netsplit)

--- src/maven_ai_engineer/commands/chat.py
+++ src/maven_ai_engineer/commands/chat.py
@@ -12,12 +12,16 @@ messages = [
 ]
 
 
-async def chat(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    messages.append({"role": "user", "content": update.message.text})
+def _chat(content):
+    messages.append({"role": "user", "content": content})
     completion = openai.chat.completions.create(model="gpt-4o-mini", messages=messages)
     completion_answer = completion.choices[0].message
     messages.append(completion_answer)
+    return completion_answer.content
 
+async def chat(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    logger.info("Received chat request:%s", update.message.text)
+    completion_answer_content = _chat(update.message.text)
     await context.bot.send_message(
-        chat_id=update.effective_chat.id, text=completion_answer.content
+        chat_id=update.effective_chat.id, text=completion_answer_content
     )
